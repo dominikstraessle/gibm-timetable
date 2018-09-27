c9cf76e3d13c5e50e75b077697f9eccd5e6ebd99
Strässle Dominik <dominik.straessle@coop.ch>
2018-09-27 10:14:52 +0200

    ajax.js: loadJobs and loadClasses with functions as parameters

---
+++ js/ajax.js
@@ -0,0 +1,48 @@
+"use strict";
+
+loadJobs(onJobsLoaded, onJobsLoadFailed, onJobsLoadDone);
+loadClasses(10, onClassesLoaded, onClassesLoadFailed, onClassesLoadDone);
+
+function onJobsLoadDone() {
+    console.log('onJobsLoadDone');
+}
+
+function onJobsLoaded(data) {
+    console.log('onJobsoaded');
+}
+
+function onJobsLoadFailed() {
+    console.log('onJobsLoadFailed');
+}
+
+function loadJobs(onLoaded, onFailed, onDone = function () {
+}) {
+    $.getJSON(
+        'http://sandbox.gibm.ch/berufe.php',
+        onLoaded
+    )
+        .fail(onFailed)
+        .done(onDone);
+}
+
+function onClassesLoadDone() {
+    console.log('onClassesLoadDone');
+}
+
+function onClassesLoaded(data) {
+    console.log('onClassesLoaded');
+}
+
+function onClassesLoadFailed() {
+    console.log('onClassesLoadFailed');
+}
+
+function loadClasses(jobId, onLoaded, onFailed, onDone = function () {
+}) {
+    $.getJSON(
+        `http://sandbox.gibm.ch/klassen.php?beruf_id=${jobId}`,
+        onLoaded
+    )
+        .fail(onFailed)
+        .done(onDone);
+}
