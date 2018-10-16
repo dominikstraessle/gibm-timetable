b13490605ac5f966c6bab063272ad3b61c5f507a
Dominik Strässle <dominik.straessle@gibmit.ch>
2018-10-16 13:36:28 +0200

    Add documentation to api methods

--- js/ajax.js
+++ js/ajax.js
@@ -3,18 +3,12 @@
 loadJobs(onJobsLoaded, onJobsLoadFailed, onJobsLoadDone);
 loadClasses(10, onClassesLoaded, onClassesLoadFailed, onClassesLoadDone);
 
-function onJobsLoadDone() {
-    console.log('onJobsLoadDone');
-}
-
-function onJobsLoaded(data) {
-    console.log('onJobsoaded: ', data);
-}
-
-function onJobsLoadFailed() {
-    console.log('onJobsLoadFailed');
-}
-
+/**
+ * Load all jobs
+ * @param onLoaded called when request was successful
+ * @param onFailed called when request failed
+ * @param onDone called every time at the end. Default does nothing.
+ */
 function loadJobs(onLoaded, onFailed, onDone = function () {
 }) {
     $.getJSON(
@@ -23,6 +17,40 @@ function loadJobs(onLoaded, onFailed, onDone = function () {
     )
         .fail(onFailed)
         .done(onDone);
+}
+
+/**
+ * Load classes of a given job
+ * @param jobId get classes of this job
+ * @param onLoaded called when request was successful
+ * @param onFailed called when request failed
+ * @param onDone called every time at the end. Default does nothing.
+ */
+function loadClasses(jobId, onLoaded, onFailed, onDone = function () {
+}) {
+    $.getJSON(
+        `http://sandbox.gibm.ch/klassen.php?beruf_id=${jobId}`,
+        onLoaded
+    )
+        .fail(onFailed)
+        .done(onDone);
+}
+
+/**
+ * Load the timetable of a given class
+ * @param classId get timetable of this class
+ * @param onLoaded called when request was successful
+ * @param onFailed called when request failed
+ * @param onDone called every time at the end. Default does nothing.
+ */
+function loadTimeTable(classId, onLoaded, onFailed, onDone = function () {
+}) {
+    $.getJSON(
+        `http://sandbox.gibm.ch/tafel.php?klasse_id=${classId}`,
+        onLoaded
+    )
+        .fail(onFailed)
+        .done(onDone());
 }
 
 function onClassesLoadDone() {
