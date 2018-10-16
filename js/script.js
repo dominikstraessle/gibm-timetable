921ecc626872ae7e50f7c7d9ad13b2a9246a44a6
Dominik Strässle <dominik.straessle@gibmit.ch>
2018-10-16 15:46:33 +0200

    Generalized and beautified methods and listeners

--- js/script.js
+++ js/script.js
@@ -5,55 +5,66 @@ $(function () {
     const classSelectId = '#classSelect';
     const timeTableId = '#timeTable';
 
-    function mapJobDataToSelectOptions(data) {
-        return data
-            .map(value => createSelectOption(value.beruf_id, value.beruf_name))
+    function getJobOptionFromObject(value) {
+        return createSelectOption(value.beruf_id, value.beruf_name);
     }
 
-    function mapClassDataToSelectOptions(data) {
+    function getClassOptionFromObject(value) {
+        return createSelectOption(value.klasse_id, value.klasse_name);
+    }
+
+    function mapDataToOption(data, getOptionFromObject) {
         return data
-            .map(value => createSelectOption(value.klasse_id, value.klasse_name))
+            .map(getOptionFromObject);
     }
 
     function showAndRefreshJobSelect(data) {
-        let options = mapJobDataToSelectOptions(data);
+        let options = mapDataToOption(data, getJobOptionFromObject);
         showSelectByIdAndRefreshOptions(jobSelectId, options);
     }
 
     function showAndRefreshClassSelect(data) {
-        let options = mapClassDataToSelectOptions(data);
+        let options = mapDataToOption(data, getClassOptionFromObject);
         showSelectByIdAndRefreshOptions(classSelectId, options);
+    }
+
+    function handleJobSelected() {
+        const selectedJobId = this.value;
+        console.debug('Selected Job: ', selectedJobId);
+        if (selectedJobId !== "") {
+            loadClasses(selectedJobId, showAndRefreshClassSelect, function () {
+                console.error('Failed to load classes');
+            });
+        } else {
+            hideElementByIdAndRemoveContent(classSelectId);
+        }
+    }
+
+    function handleClassSelected() {
+        const selectedClassId = this.value;
+        console.debug('Selected Class: ', selectedClassId);
+        if (selectedClassId !== '') {
+            loadTimeTable(selectedClassId, showTimeTable, function () {
+                console.error('Failed to load classes');
+            });
+        } else {
+            hideElementByIdAndRemoveContent(timeTableId);
+        }
     }
 
     function init() {
         hideElementByIdAndRemoveContent(classSelectId, 'fast');
 
         loadJobs(showAndRefreshJobSelect, function () {
-            console.log('failed to load jobs');
+            console.error('Failed to load jobs');
         });
 
         $(jobSelectId).on('change', function () {
-            const selectedJobId = this.value;
-            console.debug('Selected Job: ', selectedJobId);
-            if (selectedJobId !== "") {
-                loadClasses(selectedJobId, showAndRefreshClassSelect, function () {
-                    console.log('failed to load classes');
-                });
-            } else {
-                hideElementByIdAndRemoveContent(classSelectId);
-            }
+            handleJobSelected.call(this);
         });
 
         $(classSelectId).on('change', function () {
-            const selectedClassId = this.value;
-            console.debug('Selected Class: ', selectedClassId);
-            if (selectedClassId !== '') {
-                loadTimeTable(selectedClassId, showTimeTable, function () {
-                    console.error('Failed to load classes');
-                });
-            } else {
-                hideElementByIdAndRemoveContent(timeTableId);
-            }
+            handleClassSelected.call(this);
         })
     }
 
