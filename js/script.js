e7f6c3bd36f9882c72b0c8ea96e624032606f904
Dominik Strässle <dominik.straessle@gibmit.ch>
2018-10-19 07:09:33 +0200

    Add localStorage support

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ $(function () {
     const jobSelectId = '#jobSelect';
     const classSelectId = '#classSelect';
     const cardGroupId = '#cardGroup';
+    const localStorageJobKey = 'jobKey';
+    const localStorageClassKey = 'classKey';
 
     function getJobOptionFromObject(value) {
         return createSelectOption(value.beruf_id, value.beruf_name);
@@ -18,63 +20,76 @@ $(function () {
             .map(getOptionFromObject);
     }
 
+    function selectOptionByIdAndLocalStorageKey(selectId, localStorageKey) {
+        let valueFromLocalStorage = localStorage.getItem(localStorageKey);
+        if (valueFromLocalStorage !== null) {
+            $(selectId).val(valueFromLocalStorage).change();
+        }
+    }
+
     function showAndRefreshJobSelect(data) {
         let options = mapDataToElement(data, getJobOptionFromObject);
         showElementByIdAndRefreshContent(jobSelectId, options, selectContentAppender);
+        selectOptionByIdAndLocalStorageKey(jobSelectId, localStorageJobKey);
     }
 
     function showAndRefreshClassSelect(data) {
         let options = mapDataToElement(data, getClassOptionFromObject);
         showElementByIdAndRefreshContent(classSelectId, options, selectContentAppender);
+        selectOptionByIdAndLocalStorageKey(classSelectId, localStorageClassKey);
+
     }
 
-    function showTimeTable(data) {
-        console.debug(data);
+    function showTimeTableCards(data) {
         let cards = mapDataToElement(data, getCard);
         showElementByIdAndRefreshContent(cardGroupId, cards, cardGroupContentAppender);
     }
 
     function handleJobSelected() {
         const selectedJobId = this.value;
-        console.debug('Selected Job: ', selectedJobId);
         if (selectedJobId !== "") {
+            localStorage.setItem(localStorageJobKey, selectedJobId);
             loadClasses(selectedJobId, showAndRefreshClassSelect, function () {
                 console.error('Failed to load classes');
             });
             hideElementByIdAndRemoveContent(cardGroupId)
         } else {
             hideElementByIdAndRemoveContent(classSelectId);
-            hideElementByIdAndRemoveContent(cardGroupId)
+            hideElementByIdAndRemoveContent(cardGroupId);
+            localStorage.removeItem(localStorageJobKey);
         }
     }
 
     function handleClassSelected() {
         const selectedClassId = this.value;
-        console.debug('Selected Class: ', selectedClassId);
         if (selectedClassId !== '') {
-            loadTimeTable(selectedClassId, showTimeTable, function () {
+            localStorage.setItem(localStorageClassKey, selectedClassId);
+            loadTimeTable(selectedClassId, showTimeTableCards, function () {
                 console.error('Failed to load classes');
             });
         } else {
             hideElementByIdAndRemoveContent(cardGroupId);
+            localStorage.removeItem(localStorageClassKey);
         }
+    }
+
+    function initListeners() {
+        $(jobSelectId).on('change', function () {
+            handleJobSelected.call(this);
+        });
+        $(classSelectId).on('change', function () {
+            handleClassSelected.call(this);
+        });
     }
 
     function init() {
         hideElementByIdAndRemoveContent(classSelectId, 'fast');
         hideElementByIdAndRemoveContent(cardGroupId, 'fast');
+        initListeners();
 
         loadJobs(showAndRefreshJobSelect, function () {
             console.error('Failed to load jobs');
         });
-
-        $(jobSelectId).on('change', function () {
-            handleJobSelected.call(this);
-        });
-
-        $(classSelectId).on('change', function () {
-            handleClassSelected.call(this);
-        })
     }
 
     init();
